7c576d2fff78fd805ea7c433ced07a87ce3808fd
Голубев Алексей <dr.freecx@gmail.com>
2013-07-11 13:10:59 +0400

    not using

--- libimage/src/rle.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RLE_H__
-#define __RLE_H__
-
-void rle_compress( char *input, char *output, size_t in, size_t *out );
-void rle_decompress( char *input, char *output, size_t in, size_t *out );
-
-#endif
