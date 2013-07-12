b6b0f1944d81854691e05803a920bf3bdc355b79
dr.FreeCX <dr.FreeCX@gmail.com>
2013-07-12 13:59:48 +0400

    upd

--- libimage/example/test.c
+++ libimage/example/test.c
@@ -1,23 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
 #include "../src/pcx.h"
-#include "../src/tga.h"
-#include "../src/bmp.h"
 
-int main( void )
+void system_init( int *argc, char *argv[] );
+void system_opengl( void );
+void system_render( void );
+void system_resize( const int width, const int height );
+void system_destroy( void );
+
+GLuint tex;
+image_t img;
+
+void system_init( int *argc, char *argv[] )
 {
-	FILE *f;
-	image_t img;
+    glutInit( argc, argv );
+    glutInitWindowSize( 700, 300 );
+    glutInitWindowPosition( 200, 50 );
+    glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
+    glutCreateWindow( "libimage test" );
+    glutReshapeFunc( system_resize );
+    glutDisplayFunc( system_render );
+    system_opengl();
+    glutMainLoop();
+}
 
-	f = fopen( "image.pcx", "r" );
-	pcx_load( f, &img );
-	fclose( f );
-	putchar( '\n' );
-	f = fopen( "image.tga", "r" );
-	tga_load( f, &img );
-	fclose( f );
-    putchar( '\n' );
-    f = fopen( "image.bmp", "r" );
-    bmp_load( f, &img );
+void system_opengl( void )
+{
+    FILE *f;
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+    f = fopen( "image.pcx", "r" );
+    pcx_load( f, &img );
     fclose( f );
-	return 0;
+}
+
+void system_render( void )
+{
+    usleep( (1.0 / 60.0) * 1000000 );
+    glClear( GL_COLOR_BUFFER_BIT );
+    glRasterPos2f( -3.1f, 0.0f );
+    glPixelZoom( 2.0f, 2.0 );
+    glDrawPixels( img.width, img.height, GL_RGB, 
+        GL_UNSIGNED_BYTE, img.data );
+    glutSwapBuffers();
+}
+
+void system_resize( const int width, const int height )
+{
+    const float n = 1.5f;
+    const float a = (float) width / (float) height;
+
+    glViewport( 0, 0, width, height );
+    glMatrixMode( GL_PROJECTION );
+    if ( width <= height ) {
+        glOrtho( -n, n, -n / a, n / a, n, -n );
+    } else {
+        glOrtho( -n * a, n * a, -n, n, n, -n );
+    }
+    gluPerspective( 0.0f, a, 0.0f, 100.0f );
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+}
+
+void system_destroy( void )
+{
+    /* free using resource */
+}
+
+int main( int argc, char *argv[] )
+{
+    system_init( &argc, argv );
+    system_destroy();
+    return EXIT_SUCCESS;
 }
