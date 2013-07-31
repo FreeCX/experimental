a84109d8ff20efb1f0404466cefea3cce7e9c375
Голубев Алексей <dr.freecx@gmail.com>
2013-07-31 12:54:18 +0400

    argv, alpha

--- libimage/test/test.c
+++ libimage/test/test.c
@@ -13,11 +13,21 @@ void system_resize( const int width, const int height );
 void system_destroy( void );
 
 image_t img;
+float w, h;
+char def[] = "../img/image.pcx";
+char *img_file;
 
 void system_init( int *argc, char *argv[] )
 {
+    size_t len;
+    if ( *argc == 2 && ( len = strlen( argv[1] ) ) > 0 ) {
+        img_file = malloc( len );
+        strcpy( img_file, argv[1] );
+    } else {
+        img_file = def;
+    }
     glutInit( argc, argv );
-    glutInitWindowSize( 700, 300 );
+    glutInitWindowSize( 332, 50 );
     glutInitWindowPosition( 200, 50 );
     glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE );
     glutCreateWindow( "libimage test" );
@@ -27,44 +37,60 @@ void system_init( int *argc, char *argv[] )
     glutMainLoop();
 }
 
+void system_destroy( void )
+{
+    free( img_file );
+}
+
 void system_opengl( void )
 {
     FILE *f;
     glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE );
+    glEnable( GL_BLEND );
+	glEnable( GL_TEXTURE_2D );
     img_debug( 1 );
-    img_load( "../img/image.pcx", &img );
+    img_load( img_file, &img );
+    glBindTexture( GL_TEXTURE_2D, 1 );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+	glTexImage2D( GL_TEXTURE_2D, 0, img.bpp, img.width, img.height, 0,
+        img.c_format, GL_UNSIGNED_BYTE, img.data );
+	free( img.data );
 }
 
 void system_render( void )
 {
+	float n2 = img.height / h;
+	float n1 = img.width / w;
+
     usleep( (1.0 / 60.0) * 1000000 );
     glClear( GL_COLOR_BUFFER_BIT );
-    glRasterPos2f( -3.1f, 0.0f );
-    glPixelZoom( 2.0f, 2.0 );
-    glDrawPixels( img.width, img.height, GL_RGB, GL_UNSIGNED_BYTE, img.data );
+    glLoadIdentity();
+    glBegin( GL_QUADS );
+        glTexCoord2f( 1.0f, 0.0f );
+        glVertex2f( n1, -n2 );
+        glTexCoord2f( 0.0f, 0.0f );
+        glVertex2f( -n1, -n2 );
+        glTexCoord2f( 0.0f, 1.0f );
+        glVertex2f( -n1, n2 );
+        glTexCoord2f( 1.0f, 1.0f );
+        glVertex2f( n1, n2 );
+    glEnd();
     glutSwapBuffers();
 }
 
 void system_resize( const int width, const int height )
 {
-    const float n = 1.5f;
-    const float a = (float) width / (float) height;
+    float a = width / (float) height;
+    w = (float) width; h = (float) height;
 
     glViewport( 0, 0, width, height );
     glMatrixMode( GL_PROJECTION );
-    if ( width <= height ) {
-        glOrtho( -n, n, -n / a, n / a, n, -n );
-    } else {
-        glOrtho( -n * a, n * a, -n, n, n, -n );
-    }
+    glOrtho( 0.0f, width, height, 0.0f, 0.0f, 0.0f );
     gluPerspective( 0.0f, a, 0.0f, 100.0f );
     glMatrixMode( GL_MODELVIEW );
     glLoadIdentity();
-}
-
-void system_destroy( void )
-{
-    free( img.data );
 }
 
 int main( int argc, char *argv[] )
