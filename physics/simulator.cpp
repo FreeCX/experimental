8180d5990e524cf11cedff4f642e079559dfa77d
Голубев Алексей <dr.freecx@gmail.com>
2014-07-02 23:51:16 +0400

    code clean

--- physics/simulator.cpp
+++ physics/simulator.cpp
@@ -11,7 +11,6 @@ simulator::simulator( int count, int segment, float r )
     _lcount = 0;
     _r = r;
     _pobj.resize( _pcount );
-    // _pobj = new point[_pcount];
     _segment = segment;
     _vertex = new float [2*_segment];
     for ( float i = 0; i <= 180.0f; i += ( 180.0f / ( _segment - 1 ) ) ) {
@@ -24,8 +23,6 @@ simulator::simulator( int count, int segment, float r )
         _pobj[i].p = vector2( rand()%60 - 30.0f, rand()%60 - 30.0f );
         _pobj[i].v = vector2( (rand()%100 - 50.0f)/10.0f, (rand()%100 - 50.0f)/10.0f );
         _pobj[i].a = vector2( 0.0f, 0.0f );
-        // _pobj[i].a = vector2( 0.0f, -10.0f );
-        // std::cout << "a = " << _pobj[i].a << std::endl;
     }
 } 
 
@@ -34,7 +31,6 @@ simulator::~simulator()
     _pobj.clear();
     _lobj.clear();
     delete[] _vertex;
-    // delete[] _pobj;
 }
 
 void simulator::setline( const vector2 p1, const vector2 p2 )
@@ -52,19 +48,19 @@ void simulator::setline( float x0, float y0, float x1, float y1 )
 
 void simulator::p_collide( int i, int j )
 {
-    // float rad = 2.0f * _r;
-    // vector2 v1 = _pobj[i].v, v2 = _pobj[i].v;
-    // vector2 delta = _pobj[i].p - _pobj[j].p;
-    // vector2 n = ( _pobj[j].p - _pobj[i].p ).norm(), m;
+    float rad = 2.0f * _r;
+    vector2 v1 = _pobj[i].v, v2 = _pobj[i].v;
+    vector2 delta = _pobj[i].p - _pobj[j].p;
+    vector2 n = ( _pobj[j].p - _pobj[i].p ).norm(), m;
 
-    // m = vector2( n.y, -n.x );
-    // if ( delta.length() < rad ) {
-    // // if ( ( delta.length() < rad ) && ( v2 - v1 ).dot( n ) < 0 ) {
-    //     _pobj[i].v = vector2( vector2( v2.dot( n ), v1.dot( m ) ).dot( n ), 
-    //                          vector2( v2.dot( n ), v1.dot( m ) ).dot( m ) );
-    //     _pobj[j].v = vector2( vector2( v1.dot( n ), v2.dot( m ) ).dot( n ), 
-    //                          vector2( v1.dot( n ), v2.dot( m ) ).dot( m ) );
-    // } 
+    m = vector2( n.y, -n.x );
+    if ( delta.length() < rad ) {
+    // if ( ( delta.length() < rad ) && ( v2 - v1 ).dot( n ) < 0 ) {
+        _pobj[i].v = vector2( vector2( v2.dot( n ), v1.dot( m ) ).dot( n ), 
+                             vector2( v2.dot( n ), v1.dot( m ) ).dot( m ) );
+        _pobj[j].v = vector2( vector2( v1.dot( n ), v2.dot( m ) ).dot( n ), 
+                             vector2( v1.dot( n ), v2.dot( m ) ).dot( m ) );
+    } 
 }
 
 void simulator::l_collide( int i, int j )
