9c79b9f2985eb9088084afad85f015703ce97345
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-10 14:23:48 +0400

    tab to space

--- libimage/test/test.c
+++ libimage/test/test.c
@@ -19,7 +19,7 @@ int w_width = 332, w_height = 50;
 
 void system_init( int *argc, char *argv[] )
 {
-	GLint sw, sh;
+    GLint sw, sh;
     size_t len;
 
     if ( *argc == 2 && ( len = strlen( argv[1] ) ) > 0 ) {
@@ -37,7 +37,7 @@ void system_init( int *argc, char *argv[] )
     glutDisplayFunc( system_render );
     glutTimerFunc( 30, system_redraw, 0 );
     sw = glutGet( GLUT_SCREEN_WIDTH );
-	sh = glutGet( GLUT_SCREEN_HEIGHT );
+    sh = glutGet( GLUT_SCREEN_HEIGHT );
     glutPositionWindow( ( sw - w_width ) / 2, ( sh - w_height ) / 2 );
     system_opengl();
     glutMainLoop();
@@ -54,21 +54,21 @@ void system_opengl( void )
     glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
     glBlendFunc( GL_SRC_ALPHA, GL_ONE );
     glEnable( GL_BLEND );
-	glEnable( GL_TEXTURE_2D );
+    glEnable( GL_TEXTURE_2D );
     img_debug( 1 );
     img_load( img_file, &img );
     glBindTexture( GL_TEXTURE_2D, 1 );
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-	glTexImage2D( GL_TEXTURE_2D, 0, img.bpp, img.width, img.height, 0,
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexImage2D( GL_TEXTURE_2D, 0, img.bpp, img.width, img.height, 0,
         img.c_format, GL_UNSIGNED_BYTE, img.data );
-	free( img.data );
+    free( img.data );
 }
 
 void system_render( void )
 {
-	float n2 = (float) img.height / w_height;
-	float n1 = (float) img.width / w_width;
+    float n2 = (float) img.height / w_height;
+    float n1 = (float) img.width / w_width;
 
     glClear( GL_COLOR_BUFFER_BIT );
     glLoadIdentity();
