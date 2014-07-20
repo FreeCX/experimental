8ebdaa5826c96c7632084aad2a9b47e0b23f452e
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-20 12:06:02 +0400

    [upd] main func

--- gui/src/button.c
+++ gui/src/button.c
@@ -1,1 +1,79 @@
 #include "button.h"
+#include "string.h"
+
+static uint16 global_id = 0;
+ui_button_t *button_list = NULL;
+ui_button_flag_t def = { 1, 0, 0, 1, 0 };
+
+uint16 ui_button_create( uint32 x, uint32 y, uint32 w, uint32 h, const char *label, ui_button_callback func )
+{
+    size_t label_length = string_length( label );
+    ui_button_t *p = ( ui_button_t * ) calloc( sizeof( ui_button_t ) );
+    assert( p != NULL );
+    p->x_pos = x; p->y_pos = y;
+    p->width = w; p->height = h;
+    p->flag = def;
+    p->func = func;
+    if ( label_length > 0 ) {
+        p->label = (char *) malloc( label_length );
+        if ( p->label != NULL ) {
+            string_copy( label, p->label );
+        }
+    }
+    p->next = button_list;
+    button_list = p;
+    return p->id = ++global_id;
+}
+
+void ui_button_flag( uint16 id, ui_button_flag_t flag )
+{
+    ui_button_t *p = button_list;
+
+    while ( p->id != id ) {
+        if ( p->next == NULL ) {
+            return;
+        }
+        p = p->next;
+    }
+    p->flag = flag;
+}
+
+void ui_button_label( uint16 id, const char *label )
+{
+    ui_button_t *p = button_list;
+    size_t label_length = string_length( label );
+
+    while ( p->id != id ) {
+        if ( p->next == NULL ) {
+            return;
+        }
+        p = p->next;
+    }
+    if ( p->label != NULL ) {
+        free( p->label );
+    }
+    if ( label_length > 0 ) {
+        p->label = (char *) malloc( label_length );
+        if ( p->label != NULL ) {
+            string_copy( label, p->label );
+        }
+    }
+}
+
+// void ui_button_draw( ui_font *f )
+// {
+// }
+
+void ui_button_destroy( void )
+{
+    ui_button_t *p = button_list, *n;
+
+    while ( p != NULL ) {
+        n = c->next;
+        if ( p->label != NULL ) {
+            free( p->label );
+        }
+        free( c );
+        c = n;
+    }
+}
--- gui/src/button.h
+++ gui/src/button.h
@@ -1,10 +1,13 @@
 #ifndef __UI_BUTTON_H__
 #define __UI_BUTTON_H__
 
+#include <assert.h>
+#include <stdlib.h>
 #include "data.h"
 
 typedef void ( *ui_button_callback )();
 
+#pragma pack( push, 1 )
 struct ui_button_flag {
     uint8 draw     : 1;
     uint8 click    : 1;
@@ -12,16 +15,26 @@ struct ui_button_flag {
     uint8 enable   : 1;
     uint8 reserved : 4;
 };
+#pragma pack( pop )
 typedef ui_button_flag_t;
 
+#pragma pack( push, 1 )
 struct ui_button {
     uint32 x_pos, y_pos;
     uint32 width, height;
     uint16 id;
-    ui_button_flag_t flag;
     char *label;
+    ui_button_flag_t flag;
+    ui_button_callback func;
     struct ui_button *next;
 };
+#pragma pack( pop )
 typedef struct ui_button ui_button_t;
+
+uint16 ui_button_create( uint32 x, uint32 y, uint32 w, uint32 h, const char *label, ui_button_callback func );
+void ui_button_flag( uint16 id, ui_button_flag_t flag );
+void ui_button_label( uint16 id, const char *label );
+// void ui_button_draw( ui_font *f );
+void ui_button_destroy( void );
 
 #endif
