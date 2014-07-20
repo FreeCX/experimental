601f64644de62366205e47660a9efc062f1f95be
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-20 12:05:23 +0400

    [init] string functions

---
+++ gui/src/string.c
@@ -0,0 +1,24 @@
+#include "string.h"
+
+void string_copy( char *src, char *dst )
+{
+    size_t i = 0;
+
+    while ( ( dst[i++] = src[i] ) != NULL_STR );
+}
+
+void string_copy_n( char *src, char *dst, size_t start, size_t length )
+{
+    size_t i = 0;
+
+    while ( ( dst[i] = src[i+start] ) != NULL_STR && ++i <= length );
+    dst[i] = NULL_STR;
+}
+
+size_t string_length( const char *src )
+{
+    size_t i = 1;
+
+    while ( src[i++] != NULL_STR );
+    return i;
+}
---
+++ gui/src/string.h
@@ -0,0 +1,12 @@
+#ifndef __UI_STRING_H__
+#define __UI_STRING_H__
+
+#include <stdlib.h>
+
+#define NULL_STR '\0'
+
+size_t string_length( const char *src );
+void string_copy( char *src, char *dst );
+void string_copy_n( char *src, char *dst, size_t start, size_t length );
+
+#endif
