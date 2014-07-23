d6f8f89144c99db628986f2656754ead8fd478d7
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-23 12:50:15 +0400

    [add] base

--- gui/src/draw.c
+++ gui/src/draw.c
@@ -1,1 +1,34 @@
 #include "draw.h"
+
+static uint8 element_type = 0;
+static uint8 current = 0;
+static float *data = NULL;
+
+void ui_draw_begin( uint8 type )
+{
+    element_type = type;
+    if ( data == NULL ) {
+        data = (float *) malloc( DATA_SIZE * sizeof( float ) );
+    }
+}
+
+void ui_draw_vertex2i( int32 x, int32 y )
+{
+    ui_draw_vertex2f( (float) x, (float) y );
+}
+
+void ui_draw_vertex2f( float x, float y )
+{
+    if ( current < DATA_SIZE - 1 ) {
+        data[current++] = x;
+        data[current++] = y;
+    }
+}
+
+void ui_draw_end( void )
+{
+    if ( data != NULL ) {
+        // input draw code: OpenGL, SDL & etc
+        current = 0;
+    }
+}
--- gui/src/draw.h
+++ gui/src/draw.h
@@ -1,4 +1,19 @@
 #ifndef __UI_DRAW_H__
 #define __UI_DRAW_H__
 
+#include "data.h"
+
+enum {
+    UI_DRAW_POINT,
+    UI_DRAW_LINE,
+    UI_DRAW_LINE_STRIP
+};
+
+#define DATA_SIZE 100
+
+void ui_draw_begin( uint8 type );
+void ui_draw_vertex2i( int32 x, int32 y );
+void ui_draw_vertex2f( float x, float y );
+void ui_draw_end( void );
+
 #endif
