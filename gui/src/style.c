0df8fce2f2ab552d3c6b796f601c2d1559162af6
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-24 12:31:02 +0400

    [add] for load & parse *.style

---
+++ gui/src/style.c
@@ -0,0 +1,14 @@
+#include "style.h"
+#include "token.h"
+
+ui_style_t *style = NULL;
+
+void ui_style_load( const char *filename )
+{
+    // input code
+}
+
+ui_style_t *ui_style_get( void )
+{
+    return style;
+}
