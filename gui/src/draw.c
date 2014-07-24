d19fd388db3afc95da06ecb063b9a85f6b2035e7
dr.FreeCX <dr.FreeCX@gmail.com>
2014-07-24 13:05:41 +0400

    [add] render model

--- gui/src/draw.c
+++ gui/src/draw.c
@@ -1,8 +1,20 @@
 #include "draw.h"
 
+static uint8 select_render = UI_RENDER_NONE;
 static uint8 element_type = UI_DRAW_NONE;
 static uint8 current = 0;
 static float *data = NULL;
+
+uint8 ( *renders[] )( void ) = {
+    ui_null_render,
+    ui_opengl_render
+};
+size_t fmt_size = sizeof( renders ) / sizeof( renders[0] );
+
+void ui_draw_init( uint8 render )
+{
+    select_render = render;
+}
 
 void ui_draw_begin( uint8 type )
 {
@@ -22,13 +34,29 @@ void ui_draw_vertex2f( float x, float y )
     if ( current < DATA_SIZE - 1 ) {
         data[current++] = x;
         data[current++] = y;
+    } else {
+        ui_error_send( "stack overflow");
     }
 }
 
 void ui_draw_end( void )
 {
     if ( data != NULL || current != 0 || element_type != UI_DRAW_NONE ) {
-        // input draw code: OpenGL, SDL & etc
+        if ( select_render <= fmt_size ) {
+            renders[select_render](); 
+        } else {
+            ui_error_send( "unknown render" );
+        }
         current = 0;
     }
+}
+
+uint8 ui_null_render( void )
+{
+    ui_error_send( "render is not set" );
+}
+
+uint8 ui_opengl_render( void )
+{
+    // input opengl render code
 }
--- gui/src/draw.h
+++ gui/src/draw.h
@@ -2,6 +2,7 @@
 #define __UI_DRAW_H__
 
 #include "data.h"
+#include "error.h"
 
 enum {
     UI_DRAW_NONE = 0,
@@ -10,11 +11,20 @@ enum {
     UI_DRAW_LINE_STRIP
 };
 
+enum {
+    UI_RENDER_NONE = 0,
+    UI_RENDER_OPENGL
+};
+
 #define DATA_SIZE 100
 
+void ui_draw_init( uint8 render );
 void ui_draw_begin( uint8 type );
 void ui_draw_vertex2i( int32 x, int32 y );
 void ui_draw_vertex2f( float x, float y );
 void ui_draw_end( void );
+
+uint8 ui_null_render( void );
+uint8 ui_opengl_render( void );
 
 #endif
