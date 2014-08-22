d92b03a180ed77e84467a35a544c857072de917b
Голубев Алексей <dr.freecx@gmail.com>
2014-08-22 15:21:52 +0400

    [upd] structure

--- token/token.c
+++ token/token.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NULL_STR '\0'
+#define NULL_STR    '\0'
 
 struct token {
-    char *name;
-    struct token *next;
+    char **name;
+    unsigned int size;
 };
 typedef struct token token_t;
 
 char example_str[] = "special! text^= ?123;word @ number#2";
 char example_del[] = "!^?;@# ";
 
-void string_copy( char *src, char *dst, size_t start, size_t length )
+void string_copy_n( char *src, char *dst, size_t start, size_t length )
 {
     size_t i = 0;
 
     while ( ( dst[i] = src[i+start] ) != NULL_STR && ++i <= length );
-    /* if code is not working normal use this:
-        while ( i <= length ) {
-            dst[i] = src[i+start];
-            i++;
-        }
-    */
     dst[i] = NULL_STR;
 }
 
@@ -34,66 +28,70 @@ size_t string_length( const char *src )
     return i;
 }
 
-token_t *token_reverse( token_t *data )
+void tokenize( token_t **a, char *data, const char *delimeters )
 {
-    token_t *curr = data, *next;
-    data = NULL;
+    size_t start = 0, counter = 0, length = 0, n = 0;
+    size_t del_length = string_length( delimeters );
+    size_t dat_length = string_length( data );
+    size_t prev, next;
+    size_t i, j;
+    token_t *obj = NULL;
 
-    while ( curr != NULL ) {
-        next = curr->next;
-        curr->next = data;
-        data = curr;
-        curr = next;
+    obj = (token_t *) malloc( sizeof( token_t ) );
+    obj->size = next = 0;
+    prev = -1;
+    for ( i = 0; i < dat_length; i++ ) {
+        for ( j = 0; j < del_length; j++ ) {
+            if ( data[i] == delimeters[j] ) {
+                if ( next - prev > 1 ) {
+                    obj->size++;
+                }
+                prev = next;
+            }
+        }
+        next++;
     }
-    return data;
-}
-
-token_t *tokenize( char *data, const char *delimeters )
-{
-    size_t start = 0, counter = 0, length;
-    size_t i, del_length = string_length( delimeters );
-    token_t *a = NULL, *b;
-
+    obj->name = (char **) malloc( sizeof( obj->size ) * sizeof( char * ) );
     do {
         for ( i = 0; i < del_length; i++ ) {
             if ( data[counter] == delimeters[i] ) {
                 length = counter - start;
                 if ( length > 0 ) {
-                    b = (token_t *) malloc( sizeof(token_t) );
-                    b->name = (char *) malloc( length * sizeof(token_t) );
-                    string_copy( data, b->name, start, length-1 );
-                    b->next = a;
-                    a = b;
+                    obj->name[n] = (char *) malloc( ( length + 1 ) * sizeof( char ) );
+                    string_copy_n( data, obj->name[n], start, length-1 );
                     start = counter + 1;
+                    n++;
                     break;
                 }
                 start = counter + 1;
             }
         }
     } while ( data[counter++] != NULL_STR ); 
-    return token_reverse( a );
+    *a = obj;
 }
 
 void free_token( token_t *data )
 {
-    token_t *a = data, *b;
+    size_t i;
 
-    while ( a != NULL ) {
-        b = a;
-        free( a->name );
-        a = a->next;
-        free( b );
+    if ( data != NULL ) {
+        for ( i = 0; i < data->size; i++ ) {
+            free( data->name[i] );
+        }
+        free( data->name );
+        free( data );
     }
 }
 
 int main( void )
 {
-    token_t *a, *b;
+    token_t *a;
+    size_t i;
 
-    b = a = tokenize( example_str, example_del );
-    while ( b != NULL ) {
-        printf( "token: %s\n", b->name );
-        b = b->next;
+    tokenize( &a, example_str, example_del );
+    for ( i = 0; i < a->size; i++ ) {
+        printf( "token: %s\n", a->name[i] );
     }
     free_token( a );
+    return 0;
 }
