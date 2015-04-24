4dd9aff81b31583dfeea6eddc45ad828f7926e0b
Голубев Алексей <dr.freecx@gmail.com>
2015-04-24 18:13:10 +0300

    [upd] rust code

--- token/token.rs
+++ token/token.rs
@@ -1,23 +1,25 @@
+#![feature(collections)]
+
 fn tokenize( data: & str, delimeters: & str ) -> Vec< String >  {
-    let mut prev = 0u;
-    let mut next = 0u;
     let mut token: Vec< String > = Vec::new();
+    let mut prev = 0;
+    let mut next = 0;
 
     for i in data.chars() {
         for j in delimeters.chars() {
             if i == j {
-                if next - prev >= 1u {
+                if next - prev >= 1 {
                     token.push( data.slice_chars( prev, next ).to_string() );
-                    prev = next + 1u;
+                    prev = next + 1;
                     break;
                 }
-                prev = next + 1u;
+                prev = next + 1;
             }
         }
-        next += 1u;
+        next += 1;
     }
     // add last token
-    if next - prev >= 1u {
+    if next - prev >= 1 {
         token.push( data.slice_chars( prev, next ).to_string() );
     }
     return token;
@@ -28,7 +30,7 @@ fn main() {
     let del: &'static str = "!^?;@# ";
     let token = tokenize( data, del );
 
-    for i in token.iter() {
+    for i in token {
         println!( "token: {}", i );
     }
 }
