c7feb58eebd86e291a3535640dcd0876f2b5cbe8
Голубев Алексей <dr.freecx@gmail.com>
2015-06-12 12:12:04 +0300

    [upd] use unsafe slice function

--- token/token.rs
+++ token/token.rs
@@ -1,4 +1,24 @@
-#![feature(collections)]
+trait StrSpec {
+    fn slice( &self, begin: usize, end: usize ) -> &str;
+}
+
+impl StrSpec for str {
+    fn slice( &self, begin: usize, end: usize ) -> &str {
+        assert!( begin <= end );
+        let mut begin_byte;
+        let mut end_byte;
+
+        begin_byte = if begin >= 0 { Some( begin ) } else { None };
+        end_byte = if end <= self.len() { Some( end ) } else { Some( self.len() ) };
+        match ( begin_byte, end_byte ) {
+            ( None, _ ) => panic!( "slice: `begin` is beyond end of string" ),
+            ( _, None ) => panic!( "slice: `end` is beyond end of string" ),
+            ( Some( a ), Some( b ) ) => unsafe { 
+                self.slice_unchecked( a, b )
+            }
+        }
+    }
+}
 
 fn tokenize( data: & str, delimeters: & str ) -> Vec< String >  {
     let mut token: Vec< String > = Vec::new();
@@ -9,7 +29,7 @@ fn tokenize( data: & str, delimeters: & str ) -> Vec< String >  {
         for j in delimeters.chars() {
             if i == j {
                 if next - prev >= 1 {
-                    token.push( data.slice_chars( prev, next ).to_string() );
+                    token.push( data.slice( prev, next ).to_string() );
                     prev = next + 1;
                     break;
                 }
@@ -20,7 +40,7 @@ fn tokenize( data: & str, delimeters: & str ) -> Vec< String >  {
     }
     // add last token
     if next - prev >= 1 {
-        token.push( data.slice_chars( prev, next ).to_string() );
+        token.push( data.slice( prev, next ).to_string() );
     }
     return token;
 }
